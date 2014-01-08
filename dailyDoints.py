91bff4d14d3b819089761e3522e28f2fe5c2c1f4
Waleed Malik <spitfire5295@gmail.com>
2014-01-08 01:39:42 -0500

    username db sending works

--- dailyDoints.py
+++ dailyDoints.py
@@ -3,12 +3,13 @@ from snapchat import Snapchat
 f = open('username.txt', 'r')
 arr = []
 for line in f:
-	arr.append(line)
+	arr.append(line.strip('\n'))
 f.close()
 
 s = Snapchat()
 s.login('snapmecatz', 'fuckyoni')
-media_id = s.upload(Snapchat.MEDIA_IMAGE, '1.jpg')
+
 for l in arr:
+	media_id = s.upload(Snapchat.MEDIA_IMAGE, '1.jpg')
 	s.send(media_id,l)
 	print l
