1cbea74e332d385cc585a08ed3681c30abb1a28d
Tommy Lomont <tommy.lomont@gmail.com>
2014-01-09 19:18:39 -0500

    Fixing errors

--- cat.py
+++ cat.py
@@ -1,93 +1,37 @@
-
 from bottle import get, post, request, run
 from snapchat import Snapchat
 import getpass
 import urllib
 import pickle
-import re
-import urllib2
-from bs4 import BeautifulSoup
-import urllib
 
-<<<<<<< HEAD
-a=[]
-
-#TODO: change user agent string
-hdr = { 'User-Agent' : 'super happy flair bot by /u/spladug' }
-
-
-url = 'http://www.reddit.com/r/catpictures/top/'
-conn = urllib2.urlopen(urllib2.Request(url, headers=hdr))
-html = conn.read()
-soup = BeautifulSoup(html)
-for elem in soup.findAll('a', href=re.compile('\.imgur\.com/[a-zA-Z0-9]')):
-    a.append(elem['href'])
-    #print a
-
-url = 'http://www.reddit.com/r/cats/top/'
-conn = urllib2.urlopen(urllib2.Request(url, headers=hdr))
-html = conn.read()
-soup = BeautifulSoup(html)
-for elem in soup.findAll('a', href=re.compile('\.imgur\.com/[a-zA-Z0-9]')):
-    a.append(elem['href'])
-    #print elem['href']
-
-url = 'http://www.reddit.com/r/catpictures/?count=25&after=t3_1uigsy'
-conn = urllib2.urlopen(urllib2.Request(url, headers=hdr))
-html = conn.read()
-soup = BeautifulSoup(html)
-for elem in soup.findAll('a', href=re.compile('\.imgur\.com/[a-zA-Z0-9]')):
-    a.append(elem['href'])
-    #print elem['href']
-
-url = 'http://www.reddit.com/r/kittens/top/?sort=top&t=week'
-conn = urllib2.urlopen(urllib2.Request(url, headers=hdr))
-html = conn.read()
-soup = BeautifulSoup(html)
-for elem in soup.findAll('a', href=re.compile('\.imgur\.com/[a-zA-Z0-9]')):
-	a.append(elem['href'])
-	#print elem['href']
-
-url = 'http://www.reddit.com/r/lolcats/'
-conn = urllib2.urlopen(urllib2.Request(url, headers=hdr))
-html = conn.read()
-soup = BeautifulSoup(html)
-for elem in soup.findAll('a', href=re.compile('\.imgur\.com/[a-zA-Z0-9]')):
-    a.append(elem['href'])
-    #print elem['href']
-
-@get('/')
-def login():
-=======
 urls = pickle.load( open('urls.p', 'rb'))
 
 @get('/')
 def login():
-	
->>>>>>> tommy
-	return '''
-		<html style= "background-color: #E7E7E7;">
-		<head>
-		<title> SnapMeCatz! </title>
-		<link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEQARABEAAAEQEAABARAAARAQAAEBEAAAEQAAABEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDwAAwAMAAIABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIABAACBgQAAwYMAAMPDAADDwwAA5+cAAOfnAADv9wAA" rel="icon" type="image/x-icon" />	
-		<meta name="description" content="Snapchat pictures of cats to you or your friends with SnapMeCatz!">
-		<style type="text/css">
-		body{
-			font-family:  helvetica, "Tahoma";
-			font-weight: 100
-		}
-		A:visited {color: black}
+        
+        return '''
+                <html style= "background-color: #E7E7E7;">
+                <head>
+                <title> SnapMeCatz! </title>
+                <link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEQARABEAAAEQEAABARAAARAQAAEBEAAAEQAAABEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDwAAwAMAAIABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIABAACBgQAAwYMAAMPDAADDwwAA5+cAAOfnAADv9wAA" rel="icon" type="image/x-icon" />        
+                <meta name="description" content="Snapchat pictures of cats to you or your friends with SnapMeCatz!">
+                <style type="text/css">
+                body{
+                        font-family:  helvetica, "Tahoma";
+                        font-weight: 100
+                }
+                A:visited {color: black}
 A:active {text-decoration: none}
 A:hover {text-decoration: underline; color: rgb(139, 188, 190);}
-		</style>
-		</head>
-		<body>
-		<div style="margin:20px auto; text-align:center; width:400px; height: 165px; border:2px solid; border-radius:20px; background-color: white">
-		<h1 style="font-weight: 200"> SnapMeCatz!</h1>
-		<p><form name="input" action="/" method="post">
-		Your Username: 
-		<input type="text" name="username" style="border: 2px solid rgb(139, 188, 190); border-radius:5px"></p>
-		<input type="submit" value="SnapCat!" style="
+                </style>
+                </head>
+                <body>
+                <div style="margin:20px auto; text-align:center; width:400px; height: 165px; border:2px solid; border-radius:20px; background-color: white">
+                <h1 style="font-weight: 200"> SnapMeCatz!</h1>
+                <p><form name="input" action="/" method="post">
+                Your Username: 
+                <input type="text" name="username" style="border: 2px solid rgb(139, 188, 190); border-radius:5px"></p>
+                <input type="submit" value="SnapCat!" style="
     padding: 5px 15px 5px !important; 
     font-size: 14px !important; 
     background-color: rgb(139, 188, 190); font-weight: bold; 
@@ -99,11 +43,11 @@ A:hover {text-decoration: underline; color: rgb(139, 188, 190);}
     box-shadow: 0 1px 0 rgba(255, 255, 255, 0.5) inset; -moz-box-shadow: 0 1px 0 rgba(255, 255, 255, 0.5) inset; 
     -webkit-box-shadow: 0 1px 0 rgba(255, 255, 255, 0.5) inset;
     font: -webkit-body;"></p>
-		</form>
-		</div>
-		<p style="text-align:center; font-size: 14px; font-weight: 100"> Make sure to accept SnapMeCatz as a friend!</p>
-		<p style="text-align:center; font-weight:100; font-size: 14px;"> <a href="/about"> About</a></p>
-		<script>
+                </form>
+                </div>
+                <p style="text-align:center; font-size: 14px; font-weight: 100"> Make sure to accept SnapMeCatz as a friend!</p>
+                <p style="text-align:center; font-weight:100; font-size: 14px;"> <a href="/about"> About</a></p>
+                <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -112,54 +56,54 @@ A:hover {text-decoration: underline; color: rgb(139, 188, 190);}
   ga('create', 'UA-46815699-1', 'snapmecatz.com');
   ga('send', 'pageview');
 
-		</script>		
-		</body>
-		</html>
-		'''
+                </script>                
+                </body>
+                </html>
+                '''
 
 @post('/')
 def do_login():
-	from random import choice
-	url_final = choice(a)
-	while(".gif" in url_final):
-		url_final = choice(a)
-	name = 'snapmecatz'
-	password = 'fuckyoni'
-	recipient = request.forms.get('username')
-	urllib.urlretrieve(url_final, "1.jpg")
-	pic = "1.jpg"
-	s = Snapchat()
-	s.login(name, password)
+        from random import choice
+        url_final = choice(urls)
+        while(".gif" in url_final):
+                url_final = choice(urls)
+        name = 'snapmecatz'
+        password = 'fuckyoni'
+        recipient = request.forms.get('username')
+        urllib.urlretrieve(url_final, "1.jpg")
+        pic = "1.jpg"
+        s = Snapchat()
+        s.login(name, password)
 
-	#add username to file
-	#f = open('username.txt','a')
+        #add username to file
+        #f = open('username.txt','a')
         #f.write(recipient+'\n')
 
-	# Send a snapchat
-	media_id = s.upload(Snapchat.MEDIA_IMAGE, pic)
-	s.send(media_id, recipient)
+        # Send a snapchat
+        media_id = s.upload(Snapchat.MEDIA_IMAGE, pic)
+        s.send(media_id, recipient)
 
-	return '''
-		<html style=" background-color: #E7E7E7;" >
-		<head>
-		<title> SnapMeCatz! </title>
-		<link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEQARABEAAAEQEAABARAAARAQAAEBEAAAEQAAABEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDwAAwAMAAIABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIABAACBgQAAwYMAAMPDAADDwwAA5+cAAOfnAADv9wAA" rel="icon" type="image/x-icon" />
-		<style type="text/css">
-		body{
-			font-family:  helvetica, "Tahoma";
-			font-weight: 100;
-		}
-		A:visited {color: black}
+        return '''
+                <html style=" background-color: #E7E7E7;" >
+                <head>
+                <title> SnapMeCatz! </title>
+                <link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEQARABEAAAEQEAABARAAARAQAAEBEAAAEQAAABEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDwAAwAMAAIABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIABAACBgQAAwYMAAMPDAADDwwAA5+cAAOfnAADv9wAA" rel="icon" type="image/x-icon" />
+                <style type="text/css">
+                body{
+                        font-family:  helvetica, "Tahoma";
+                        font-weight: 100;
+                }
+                A:visited {color: black}
 A:active {text-decoration: none}
 A:hover {text-decoration: underline; color: rgb(139, 188, 190);}
-		</style>
-		</head>
-		<body>
-		<div style="margin:20px auto; text-align:center; width:400px; height: 165px">
-		<h1 style="font-weight: 200"> SnapCat sent!</h1>
-		<div>
-		<p style="font-weight:100"> <a href="../">Return Home</a></p>
-		<script>
+                </style>
+                </head>
+                <body>
+                <div style="margin:20px auto; text-align:center; width:400px; height: 165px">
+                <h1 style="font-weight: 200"> SnapCat sent!</h1>
+                <div>
+                <p style="font-weight:100"> <a href="../">Return Home</a></p>
+                <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -169,21 +113,21 @@ A:hover {text-decoration: underline; color: rgb(139, 188, 190);}
   ga('send', 'pageview');
 
 </script>
-		</body>
-		</html>
-		'''
+                </body>
+                </html>
+                '''
 
 @get('/about')
 def about():
-	return '''
-		<html style=" background-color: #E7E7E7;" >
+        return '''
+                <html style=" background-color: #E7E7E7;" >
 <head>
 <title> SnapMeCatz! | About </title>
 <link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEQARABEAAAEQEAABARAAARAQAAEBEAAAEQAAABEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDwAAwAMAAIABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIABAACBgQAAwYMAAMPDAADDwwAA5+cAAOfnAADv9wAA" rel="icon" type="image/x-icon" />
 <style type="text/css">
 body{
-	font-weight: 100;
-	font-family:  helvetica, "Tahoma"
+        font-weight: 100;
+        font-family:  helvetica, "Tahoma"
 }
 A:visited {color: black}
 A:active {text-decoration: none}
@@ -215,4 +159,4 @@ A:hover {text-decoration: underline; color: rgb(139, 188, 190);}
 '''
 
 
-run(host='localhost', port=8080)
+run(host='0.0.0.0', port=80)
