c722a67e3542b5853b62e414bbca9f7a4da5e22a
Waleed Malik <spitfire5295@gmail.com>
2015-01-26 00:14:46 -0500

    update snapchat vers

--- snapchat.py
+++ snapchat.py
@@ -19,7 +19,7 @@ class Snapchat:
     BLOB_ENCRYPTION_KEY =   'M02cnQ51Ji97vwT4'                          # Blob Encryption Key
     HASH_PATTERN =          '0001110111101110001111010101111011010001001110011000110001000110'; # Hash pattern
     USERAGENT =             'Snapchat/8.0.1.3 (Nexus 5; Android 21; gzip)'  # The default useragent
-    SNAPCHAT_VERSION =      'Snapcaht/8.0.1.3'                                     # Snapchat Application Version
+    SNAPCHAT_VERSION =      'Snapchat/8.0.1.3'                                     # Snapchat Application Version
 
     MEDIA_IMAGE =                        0  # Media: Image
     MEDIA_VIDEO =                        1  # Media: Video
